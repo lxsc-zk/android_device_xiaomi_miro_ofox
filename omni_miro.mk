9e01475b42c7ba252e67d845a9329ebf49349d3c
lxsc-zk <2794618934@qq.com>
2026-07-31 11:08:10 +0800

    Fix device codename to miro and adapt OrangeFox 14.1 vars

---
+++ omni_miro.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2026 The Android Open Source Project
+# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from qcom device
+$(call inherit-product, device/xiaomi/qcom/device.mk)
+
+PRODUCT_DEVICE := miro
+PRODUCT_NAME := omni_miro
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Redmi K80 Pro
+PRODUCT_MANUFACTURER := xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="twrp_miro-eng 99.87.36 BP2A.250605.031.A2 eng.lu test-keys"
+
+BUILD_FINGERPRINT := Xiaomi/twrp_miro/qcom:99.87.36/BP2A.250605.031.A2/eng.lu:eng/test-keys
---
+++ vendorsetup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# 1. 基础构建环境
+export ALLOW_MISSING_DEPENDENCIES=true
+export LC_ALL="C"
+export FOX_BUILD_TYPE="Unofficial"
+export FOX_MAINTAINER_PATCH_VERSION="1"
+
+# 2. 瘦身与体积压缩 (针对 8e/A15 分区空间溢出优化)
+export OF_USE_LZMA_COMPRESSION=1
+export FOX_DRASTIC_SIZE_REDUCTION=1
+export OF_DISABLE_MIUI_SPECIFIC_FEATURES=1
+
+# 3. 骁龙 8 Elite (miro) / Android 15 架构声明
+export FOX_AB_DEVICE=1
+export FOX_VIRTUAL_AB_DEVICE=1
+
+# 4. FBEv2 硬件解密版本
+export OF_DEFAULT_KEYMASTER_VERSION=4.1
+
+# 5. 注册 Lunch 选项
+add_lunch_combo omni_miro-userdebug
+add_lunch_combo omni_miro-eng
